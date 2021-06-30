9df79e66697f1e43e7688bd0d63ea549062ca4f0
lomucode <65655031+lomucode@users.noreply.github.com>
2021-06-30 16:53:42 +0200

    FrontEnd Updates
    - Update Password
    - CarouselView in Dashboard
    - Little changes in Frontend
    - Data transfer from online server

--- AthenathonApp/AthenathonApp.Android/obj/Debug/android/environment.x86_64.s
+++ AthenathonApp/AthenathonApp.Android/obj/Debug/android/environment.x86_64.s
@@ -72,7 +72,7 @@ mono_aot_mode_name:
 	.section	.rodata.env.str.8,"aMS",@progbits,1
 	.type	.L.env.str.8, @object
 .L.env.str.8:
-	.asciz	"02087fb5-56d8-4214-b84e-1d84129ab79d"
+	.asciz	"d4f92c72-3f9e-4d07-a8f2-905b287c78cd"
 	.size	.L.env.str.8, 37
 	.section	.rodata.env.str.9,"aMS",@progbits,1
 	.type	.L.env.str.9, @object
---
+++ AthenathonApp/AthenathonApp/Views/ChangePassword.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+using Xamarin.Forms.PlatformConfiguration;
+using Xamarin.Forms.PlatformConfiguration.iOSSpecific;
+
+namespace AthenathonApp.Views
+{
+    public partial class ChangePassword : ContentPage
+    {
+
+        public ChangePassword()
+        {
+            InitializeComponent();
+        }
+
+        public async void BackButton(object sender, System.EventArgs e)
+        {
+            await Navigation.PushModalAsync(new Framework());
+        }
+
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            var safeInsets = On<iOS>().SafeAreaInsets();
+            safeInsets.Left = 0;
+            Padding = safeInsets;
+        }
+    }
+}
