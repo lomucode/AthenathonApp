f072b1161c5121891c1ce85dd07f1e7f85d69e4d
olispcvs <83537940+olispcvs@users.noreply.github.com>
2021-07-08 09:38:41 +0200

    GPS Up2Date
    Bitte Xamarin.Community.Toolkit installieren, falls nötig

--- AthenathonApp/AthenathonApp.Android/obj/Debug/android/environment.armeabi-v7a.s
+++ AthenathonApp/AthenathonApp.Android/obj/Debug/android/environment.armeabi-v7a.s
@@ -92,7 +92,7 @@ mono_aot_mode_name:
 	.section	.rodata.env.str.8,"aMS",%progbits,1
 	.type	.L.env.str.8, %object
 .L.env.str.8:
-	.asciz	"d4f92c72-3f9e-4d07-a8f2-905b287c78cd"
+	.asciz	"56da7e7e-cb64-4a00-9121-7ea96fb570a9"
 	.size	.L.env.str.8, 37
 	.section	.rodata.env.str.9,"aMS",%progbits,1
 	.type	.L.env.str.9, %object
